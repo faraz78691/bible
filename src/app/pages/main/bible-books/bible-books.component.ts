1230c9fd7c8775b8d57c2ee9db4c8b2dff89662c
palash <palash29jain@gmail.com>
2024-05-16 20:17:42 +0530

    footer and add friend

--- src/app/pages/main/bible-books/bible-books.component.ts
+++ src/app/pages/main/bible-books/bible-books.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-bible-books',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './bible-books.component.html',
   styleUrls: ['./bible-books.component.css']
 })
--- src/app/pages/main/member-area/member-area.component.ts
+++ src/app/pages/main/member-area/member-area.component.ts
@@ -1,25 +1,30 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable, Subscription, map, tap } from 'rxjs';
 import { ApiService } from 'src/app/services/api.service';
 import { LoaderService } from 'src/app/services/loader.service';
 import { SafeHtmlPipe } from 'src/app/helper/safe-html.pipe';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-member-area',
   standalone: true,
-  imports: [CommonModule, SafeHtmlPipe],
+  imports: [CommonModule, SafeHtmlPipe, FormsModule, ReactiveFormsModule, FooterComponent],
   templateUrl: './member-area.component.html',
   styleUrls: ['./member-area.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
 export class MemberAreaComponent {
 
+  form!: FormGroup;
+  @ViewChild('closeModal') closeModal!: ElementRef;
+
   // savedverse$!: Observable<any>;
   savedverse$!: Observable<any>;
   editedverse$!: Observable<any>;
-  notedverse: any[]=[]
- 
+  notedverse: any[] = []
+
   verseOftheDay: any = {};
   notes: string = '';
   constructor(private loaderService: LoaderService, private apiService: ApiService) { }
@@ -28,12 +33,50 @@ export class MemberAreaComponent {
   ngOnInit(): void {
 
 
-    this.savedverse$ = this.apiService.getApi('getSavedVerses').pipe(tap(items=>
-      this.notedverse = (items.data).filter((val:any)=>val.notes !='')))
-    
+    this.savedverse$ = this.apiService.getApi('getSavedVerses').pipe(tap(items =>
+      this.notedverse = (items.data).filter((val: any) => val.notes != '')))
+
     this.editedverse$ = this.apiService.getApi('getEditedVerses');
-  
+    this.initForm();
 
     // this.loaderService.removeLoaderClass();
   };
+
+  initForm() {
+    this.form = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    })
+  }
+
+  addFriend() {
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      //this.loading = true;
+      const formURlData = new URLSearchParams();
+      formURlData.set('name', this.form.value.name);
+      formURlData.set('email', this.form.value.email);
+      this.apiService.postAPI('addFriends', formURlData.toString()).subscribe({
+        next: (resp) => {
+          console.log(resp)
+          if (resp.success == true) {
+            this.apiService.showSuccess(resp.message);
+            this.closeModal.nativeElement.click();
+            //this.loading = false;
+            this.form.reset();
+          } else {
+            this.apiService.showWarning(resp.message);
+            //this.loading = false;
+          }
+        },
+        error: (error) => {
+          //this.loading = false;
+          this.apiService.showError('Something went wrong.');
+          console.error('Login error:', error.message);
+        }
+      });
+    }
+  }
+
+
 }
